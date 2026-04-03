0d0cb23a551154bdf4208a436aebef50212a319e
Lucia Alcaraz <pqsi2017@gmail.com>
2026-04-03 09:35:30 +0100

    Update delete.sql

--- sql/delete.sql
+++ sql/delete.sql
@@ -1,3 +1,7 @@
---Elimina el usuario con id 9
-DELETE FROM Form
-WHERE id = 9;
+-- Eliminar todos los registros con edad 30
+DELETE FROM form
+WHERE age = 30;
+
+-- Eliminar todos los registros con nombre pedro
+DELETE FROM form
+WHERE name = 'pedro';
